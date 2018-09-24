33f6315ddd5ac8e9220912953f6e89cc9d329db2
gegorov <gegorov@yahoo.com>
2018-09-25 01:39:36 +0300

    Brackets: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  if (str.length <= 0) {
+    return true;
+  }
+  if (str.length % 2 !== 0) {
+    return false;
+  }
+  const brackets = new Map();
+
+  bracketsConfig.forEach((element) => {
+    brackets.set(element[0], element[1]);
+  });
+
+  const myStr = str;
+  const stack = [];
+  let isCorrect = false;
+
+  myStr.split('').forEach((char) => {
+    const previous = brackets.get(stack[stack.length - 1]);
+    if (brackets.has(char) && previous !== char) {
+      stack.push(char);
+      isCorrect = true;
+    } else if (stack.length > 0 && previous === char) {
+      stack.pop();
+      isCorrect = true;
+    } else {
+      isCorrect = false;
+    }
+  });
+  if (stack.length > 0) {
+    isCorrect = false;
+  }
+
+  return isCorrect;
+};
